6b13988ee2fa98afa3b2671a028ecc4e78b44578
Ahmed <Ahmednasrmahmoud6@gmail.com>
2024-03-11 09:17:40 -0400

    first push

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  accessToken: 'pk.eyJ1IjoidHJpc2tvZGFyZGVuIiwiYSI6ImNsdGduNmtwdzEyNXEyaXJ6amhlYzA0ZjQifQ.r_1H6T4Ungw60N0S3sMt2w',
+  style: 'mapbox://styles/mapbox/light-v11',
+  CrimesURL: 'mapbox://triskodarden.b7pll4wx',
+};
