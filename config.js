e8a20f93b305cefec23616bfaae09bbb041091cf
Ahmed <Ahmednasrmahmoud6@gmail.com>
2024-03-24 23:30:20 -0400

    Added all three layers

--- config.js
+++ config.js
@@ -2,4 +2,9 @@ var config = {
   accessToken: 'pk.eyJ1IjoidHJpc2tvZGFyZGVuIiwiYSI6ImNsdGduNmtwdzEyNXEyaXJ6amhlYzA0ZjQifQ.r_1H6T4Ungw60N0S3sMt2w',
   style: 'mapbox://styles/mapbox/light-v11',
   CrimesURL: 'mapbox://triskodarden.b7pll4wx',
+  CrimesSource: 'Crimes_Geolocations-af0gkk',
+  InterestURL: 'mapbox://triskodarden.9lxywazx',
+  InterestSource: 'Points_of_Interest-0oqasw',
+  TrialsURL: 'mapbox://triskodarden.752208ku',
+  TrialsSource: 'Trials_Geolocations-8dwcwr',
 };
