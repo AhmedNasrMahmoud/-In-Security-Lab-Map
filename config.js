04575f8d387af8b4161c635a8117e94e5f65b178
Ahmed <Ahmednasrmahmoud6@gmail.com>
2024-03-31 23:49:50 -0400

    Modified the popup and fixed some bugs that it had

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
   accessToken: 'pk.eyJ1IjoidHJpc2tvZGFyZGVuIiwiYSI6ImNsdGduNmtwdzEyNXEyaXJ6amhlYzA0ZjQifQ.r_1H6T4Ungw60N0S3sMt2w',
   style: 'mapbox://styles/mapbox/light-v11',
-  CrimesURL: 'mapbox://triskodarden.b7pll4wx',
-  CrimesSource: 'Crimes_Geolocations-af0gkk',
-  InterestURL: 'mapbox://triskodarden.9lxywazx',
-  InterestSource: 'Points_of_Interest-0oqasw',
-  TrialsURL: 'mapbox://triskodarden.752208ku',
-  TrialsSource: 'Trials_Geolocations-8dwcwr',
+  CrimesURL: 'mapbox://triskodarden.2s5k3jqw',
+  CrimesSource: 'Crimes_Geolocations-5gefgy',
+  InterestURL: 'mapbox://triskodarden.6i0czh0k',
+  InterestSource: 'Points_of_Interest-04y3qv',
+  TrialsURL: 'mapbox://triskodarden.daegn9mv',
+  TrialsSource: 'Trials_Geolocations-5yiru1',
 };
